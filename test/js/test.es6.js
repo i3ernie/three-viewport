039375146366ce64e7fca211a2a3c233bf6d98af
bernie <mail@javaner.de>
2021-08-18 11:37:24 +0200

    move to /js

--- test/js/test.es6.js
+++ test/js/test.es6.js
@@ -1,6 +1,6 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from "../../node_modules/three/build/three.module.js";
 import WoodBox from "./WoodBox.js";
-import Viewport from "../src/Viewport.js";
+import Viewport from "../../src/Viewport.js";
 
 
     let el = document.getElementById("viewport");
